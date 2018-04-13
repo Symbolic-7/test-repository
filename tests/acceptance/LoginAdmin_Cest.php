38dece69c948474077258083b383b330761215d5
Gusev Anton <antongusev@bk.ru>
2018-04-13 16:10:02 +0300

    update

--- tests/acceptance/LoginAdmin_Cest.php
+++ tests/acceptance/LoginAdmin_Cest.php
@@ -14,16 +14,12 @@ class LoginAdmin_Cest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
-  /*      $I->amOnPage('admin/');
+        $I->amOnPage('admin/');
         $I->waitForElement(['class' => 'input-wrapper'],30);
         $I->fillField('username', 'admin');
         $I->fillField('password', 'admin');
         $I->click(['name' => 'login']);
         $I->waitForElement(['id' => 'sidebar']);
         $I->seeElement(['id' => 'widget-stats']);
-  */
-
-  $I->amOnUrl('https://codeception.com/docs/modules/WebDriver#Selenium');
-  $I->wait(60);
     }
 }
